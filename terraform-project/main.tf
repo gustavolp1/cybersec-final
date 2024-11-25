afc6ddadfe83fe8aa4e93c86a18f45b1503a2841
gustavolp1 <gustavolp1@al.insper.edu.br>
2024-11-24 21:21:42 -0300

    a

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -2,21 +2,95 @@ provider "aws" {
   region = var.region
 }
 
+# VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MyVPC"
+  }
 }
 
+# Sub-redes
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "PublicSubnet"
+  }
 }
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "PrivateSubnet"
+  }
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "MainInternetGateway"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route" "public_to_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+  tags = {
+    Name = "MyNATGateway"
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "PrivateRouteTable"
+  }
+}
+
+resource "aws_route" "private_to_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+# Security Groups
 resource "aws_security_group" "development" {
   vpc_id = aws_vpc.main.id
 
@@ -24,7 +98,7 @@ resource "aws_security_group" "development" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["177.170.240.204/32"] # Adicionar mais IPs se necessário
+    cidr_blocks = [var.my_ip] # Seu IP público
   }
 
   egress {
@@ -32,6 +106,10 @@ resource "aws_security_group" "development" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "DevelopmentSecurityGroup"
   }
 }
 
@@ -51,20 +129,81 @@ resource "aws_security_group" "database" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "DatabaseSecurityGroup"
+  }
 }
 
+# EC2 Instances
 resource "aws_instance" "development" {
-  ami           = "ami-01ebf7c0e446f85f9"
-  instance_type = "t2.micro"
+  ami           = "ami-00eb69d236edcfaf8"
+  instance_type = "t3.micro"
   subnet_id     = aws_subnet.public.id
-  security_groups = [aws_security_group.development.name]
+  vpc_security_group_ids = [aws_security_group.development.id]
   key_name      = "dev-keypair"
+
+  tags = {
+    Name = "DevelopmentInstance"
+  }
 }
 
 resource "aws_instance" "database" {
-  ami           = "ami-01ebf7c0e446f85f9"
-  instance_type = "t2.micro"
+  ami           = "ami-00eb69d236edcfaf8"
+  instance_type = "t3.micro"
   subnet_id     = aws_subnet.private.id
-  security_groups = [aws_security_group.database.name]
+  vpc_security_group_ids = [aws_security_group.database.id]
   key_name      = "dev-keypair"
+
+  tags = {
+    Name = "DatabaseInstance"
+  }
+}
+
+resource "aws_instance" "zabbix_server" {
+  ami           = "ami-00d5c4dd05b5467c4"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.zabbix.id]
+  key_name      = "dev-keypair"
+
+  tags = {
+    Name = "ZabbixServer"
+  }
+}
+
+resource "aws_security_group" "zabbix" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 10051
+    to_port     = 10051
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ZabbixSecurityGroup"
+  }
 }
--- terraform-project/variables.tf
+++ terraform-project/variables.tf
@@ -1,3 +1,7 @@
 variable "region" {
   default = "us-east-2"
 }
+
+variable "my_ip" {
+  default = "177.170.240.204/32"
+}
